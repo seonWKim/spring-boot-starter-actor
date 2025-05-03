92c9355af950532bd68ce1c7130b389f5c4fa655
김선우 <seonwoo960000@toss.im>
2025-05-03 17:07:15 +0900

    Change dependency from web to webflux

--- example/simple/build.gradle.kts
+++ example/simple/build.gradle.kts
@@ -2,7 +2,7 @@ dependencies {
 	implementation(project(":core"))
 
 	implementation("org.springframework.boot:spring-boot-starter")
-	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
 
 	implementation("org.springframework.boot:spring-boot-starter")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
