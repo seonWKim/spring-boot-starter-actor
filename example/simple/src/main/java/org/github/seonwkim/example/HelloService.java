d88e01f2349e55742dfd5decccae4e3af095a06a
김선우 <seonwoo960000@toss.im>
2025-05-06 19:30:44 +0900

    Add example that make use of the actorSystem

--- example/simple/src/main/java/org/github/seonwkim/example/HelloService.java
+++ example/simple/src/main/java/org/github/seonwkim/example/HelloService.java
@@ -2,10 +2,8 @@ package org.github.seonwkim.example;
 
 import java.time.Duration;
 import java.util.concurrent.CompletionStage;
-import java.util.concurrent.ExecutionException;
 
 import org.apache.pekko.actor.typed.ActorRef;
-import org.apache.pekko.actor.typed.Scheduler;
 import org.apache.pekko.actor.typed.javadsl.AskPattern;
 import org.github.seonwkim.core.ActorSystemInstance;
 import org.springframework.stereotype.Service;
@@ -18,8 +16,7 @@ public class HelloService {
     private final ActorSystemInstance actorSystemInstance;
     private final CompletionStage<ActorRef<HelloActor.Command>> helloActor;
 
-    public HelloService(ActorSystemInstance actorSystemInstance)
-            throws ExecutionException, InterruptedException {
+    public HelloService(ActorSystemInstance actorSystemInstance) {
         this.actorSystemInstance = actorSystemInstance;
         this.helloActor = actorSystemInstance.spawn(HelloActor.Command.class, "hello");
     }
@@ -30,7 +27,6 @@ public class HelloService {
                         .thenCompose(actorRef ->
                                              AskPattern.ask(actorRef,
                                                             HelloActor.SayHello::new,
-                                                            // builds SayHello(replyTo)
                                                             Duration.ofSeconds(3),
                                                             actorSystemInstance.getRaw().scheduler()
                                              )
