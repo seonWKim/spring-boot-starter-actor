ae712596814e4b34c4aed678008c697626146160
김선우 <seonwoo960000@toss.im>
2025-05-09 00:00:46 +0900

    Remove deprecated modules

--- core-deprecated/src/main/java/org/github/seonwkim/core/ActorRefWrapper.java
+++
@@ -1,9 +0,0 @@
-package org.github.seonwkim.core;
-
-import org.apache.pekko.actor.typed.ActorRef;
-
-public interface ActorRefWrapper<T> {
-	String beanName();
-
-	ActorRef<T> unwrap();
-}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,5 @@
 rootProject.name = "spring-pekko"
 
 include("core")
-include("core-deprecated")
-
-// examples
 include("example:simple")
 include("example:cluster")
-include("example-deprecated:simple")
-include("example-deprecated:cluster")
