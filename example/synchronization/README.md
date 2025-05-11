d478a209c6014b462a03026e8fb39e6e13e4cfd7
김선우 <seonwoo960000@toss.im>
2025-05-11 09:18:14 +0900

    Initialize synchronization module under example

---
+++ example/synchronization/README.md
@@ -0,0 +1,11 @@
+# Synchronization Example 
+
+An example on how to synchronize using actors 
+
+## Synchronization Methods 
+
+### DB Locking 
+
+## Redis Locking 
+
+## Actor Locking 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,8 @@ rootProject.name = "spring-boot-starter-actor"
 
 include(":core")
 include(":core-boot3")
+
 include(":example:chat")
 include(":example:cluster")
 include(":example:simple")
+include(":example:synchronization")
