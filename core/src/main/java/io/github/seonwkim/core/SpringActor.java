a60d927ca90437b14b0dd8236eb08efb68093571
Kim Seon Woo <69591622+seonwoo960000@users.noreply.github.com>
2025-05-30 06:59:58 +0900

    Change SpringActor to receive a generic type

--- core/src/main/java/io/github/seonwkim/core/SpringActor.java
+++ core/src/main/java/io/github/seonwkim/core/SpringActor.java
@@ -5,15 +5,18 @@ import org.apache.pekko.actor.typed.Behavior;
 /**
  * Interface for Spring-managed actors. Classes implementing this interface will be automatically
  * registered with the actor system.
+ *
+ * The purpose of forcing the return type of the create method to the actor's command class is
+ * for simplicity(e.g. Multiple SpringActor's implementing the same class will increase overall complexity).
  */
-public interface SpringActor {
+public interface SpringActor<T> {
 	/**
 	 * Returns the class of commands that this actor can handle. This is used to register the actor
 	 * with the ActorTypeRegistry.
 	 *
 	 * @return The class of commands that this actor can handle
 	 */
-	Class<?> commandClass();
+	Class<T> commandClass();
 
 	/**
 	 * Creates a behavior for this actor. This method is called by the actor system when a new actor
@@ -22,5 +25,5 @@ public interface SpringActor {
 	 * @param actorContext The context of the actor
 	 * @return A behavior for the actor
 	 */
-	Behavior<?> create(SpringActorContext actorContext);
+	Behavior<T> create(SpringActorContext actorContext);
 }
