002fb56a3a1a793f381e234fc573d11988594218
김선우 <seonwoo960000@toss.im>
2025-06-05 07:44:36 +0900

    Nit

--- example/chat/src/main/java/io/github/seonwkim/example/ClusterShardingMetricsActor.java
+++ example/chat/src/main/java/io/github/seonwkim/example/ClusterShardingMetricsActor.java
@@ -1,26 +1,32 @@
 package io.github.seonwkim.example;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import org.apache.pekko.actor.typed.Behavior;
 import org.apache.pekko.actor.typed.javadsl.Behaviors;
-import org.apache.pekko.cluster.sharding.ShardRegion.ClusterShardingStats;
+import org.apache.pekko.cluster.sharding.ShardRegion.CurrentShardRegionState;
 import org.springframework.stereotype.Component;
 
 import io.github.seonwkim.core.SpringActor;
 import io.github.seonwkim.core.SpringActorContext;
-import io.github.seonwkim.core.SpringActorSystem;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tags;
 
 @Component
 public class ClusterShardingMetricsActor
-        implements SpringActor<ClusterShardingMetricsActor, ClusterShardingStats> {
+        implements SpringActor<ClusterShardingMetricsActor, CurrentShardRegionState> {
 
     public static class ClusterShardingMetricsExporterContext implements SpringActorContext {
 
         private final String actorId;
-        private final SpringActorSystem springActorSystem;
+        private final MeterRegistry meterRegistry;
 
-        public ClusterShardingMetricsExporterContext(String actorId, SpringActorSystem springActorSystem) {
+        public ClusterShardingMetricsExporterContext(
+                String actorId, MeterRegistry meterRegistry) {
             this.actorId = actorId;
-            this.springActorSystem = springActorSystem;
+            this.meterRegistry = meterRegistry;
         }
 
         @Override
@@ -30,23 +36,36 @@ public class ClusterShardingMetricsActor
     }
 
     @Override
-    public Behavior<ClusterShardingStats> create(SpringActorContext actorContext) {
+    public Behavior<CurrentShardRegionState> create(SpringActorContext actorContext) {
         if (!(actorContext instanceof ClusterShardingMetricsExporterContext)) {
             throw new IllegalStateException("Must be ClusterShardingMetricsExporterContext");
         }
 
         final ClusterShardingMetricsExporterContext context =
                 (ClusterShardingMetricsExporterContext) actorContext;
-        return Behaviors.setup(ctx -> Behaviors.receive(ClusterShardingStats.class)
-                                               .onMessage(ClusterShardingStats.class,
-                                                          msg -> handle(msg, context.springActorSystem))
+        return Behaviors.setup(ctx -> Behaviors.receive(CurrentShardRegionState.class)
+                                               .onMessage(CurrentShardRegionState.class,
+                                                          msg -> handle(msg, context.meterRegistry))
                                                .build()
         );
     }
 
-    private Behavior<ClusterShardingStats> handle(ClusterShardingStats msg,
-                                                  SpringActorSystem springActorSystem) {
-        System.out.println("MSG: " + msg);
+    private final Map<String, AtomicInteger> shardEntityCounters = new ConcurrentHashMap<>();
+
+    private Behavior<CurrentShardRegionState> handle(CurrentShardRegionState msg,
+                                                     MeterRegistry meterRegistry) {
+        msg.shards().foreach(shard -> {
+            final String shardId = shard.shardId();
+            final int entitiesCount = shard.entityIds().size();
+            shardEntityCounters.computeIfAbsent(shardId, key -> {
+                AtomicInteger gauge = new AtomicInteger(entitiesCount);
+                meterRegistry.gauge("pekko.shard.entities", Tags.of("shardId", shardId), gauge);
+                return gauge;
+            }).set(entitiesCount);
+
+            return null;
+        });
+
         return Behaviors.same();
     }
 }
--- example/chat/src/main/java/io/github/seonwkim/example/ClusterShardingMetricsExporter.java
+++ example/chat/src/main/java/io/github/seonwkim/example/ClusterShardingMetricsExporter.java
@@ -1,6 +1,5 @@
 package io.github.seonwkim.example;
 
-import java.time.Duration;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -8,45 +7,34 @@ import java.util.concurrent.TimeUnit;
 import javax.annotation.PostConstruct;
 
 import org.apache.pekko.actor.typed.ActorRef;
-import org.apache.pekko.cluster.sharding.ShardRegion.ClusterShardingStats;
 import org.apache.pekko.cluster.sharding.ShardRegion.CurrentShardRegionState;
 import org.apache.pekko.cluster.sharding.typed.ClusterShardingQuery;
-import org.apache.pekko.cluster.sharding.typed.GetClusterShardingStats;
 import org.apache.pekko.cluster.sharding.typed.GetShardRegionState;
-import org.apache.pekko.cluster.sharding.typed.javadsl.EntityTypeKey;
 import org.springframework.stereotype.Component;
 
 import io.github.seonwkim.core.SpringActorRef;
 import io.github.seonwkim.core.SpringActorSpawnContext;
 import io.github.seonwkim.core.SpringActorSystem;
 import io.github.seonwkim.example.ClusterShardingMetricsActor.ClusterShardingMetricsExporterContext;
-import io.github.seonwkim.example.ClusterShardingMetricsActorV2.ClusterShardingMetricsExporterV2Context;
+import io.micrometer.core.instrument.MeterRegistry;
 
 @Component
 public class ClusterShardingMetricsExporter {
 
     private final SpringActorSystem springActorSystem;
-    private final SpringActorRef<ClusterShardingStats> metricsActors;
-    private final SpringActorRef<CurrentShardRegionState> metricsActorsV2;
+    private final SpringActorRef<CurrentShardRegionState> metricsActors;
 
     private final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
 
-    public ClusterShardingMetricsExporter(SpringActorSystem springActorSystem) {
+    public ClusterShardingMetricsExporter(MeterRegistry meterRegistry, SpringActorSystem springActorSystem) {
         this.springActorSystem = springActorSystem;
-
-        final SpringActorSpawnContext<ClusterShardingMetricsActor, ClusterShardingStats> context =
-                new SpringActorSpawnContext.Builder<ClusterShardingMetricsActor, ClusterShardingStats>()
+        final SpringActorSpawnContext<ClusterShardingMetricsActor, CurrentShardRegionState> context =
+                new SpringActorSpawnContext.Builder<ClusterShardingMetricsActor, CurrentShardRegionState>()
                         .actorClass(ClusterShardingMetricsActor.class)
-                        .actorContext(new ClusterShardingMetricsExporterContext("exporter", springActorSystem))
-                        .build();
-        final SpringActorSpawnContext<ClusterShardingMetricsActorV2, CurrentShardRegionState> contextV2 =
-                new SpringActorSpawnContext.Builder<ClusterShardingMetricsActorV2, CurrentShardRegionState>()
-                        .actorClass(ClusterShardingMetricsActorV2.class)
-                        .actorContext(new ClusterShardingMetricsExporterV2Context("exporter", springActorSystem))
+                        .actorContext(new ClusterShardingMetricsExporterContext("exporter", meterRegistry))
                         .build();
 
         this.metricsActors = springActorSystem.spawn(context).toCompletableFuture().join();
-        this.metricsActorsV2 = springActorSystem.spawn(contextV2).toCompletableFuture().join();
     }
 
     @PostConstruct
@@ -54,17 +42,10 @@ public class ClusterShardingMetricsExporter {
         final ActorRef<ClusterShardingQuery> shardStateActor =
                 springActorSystem.getClusterSharding().shardState();
 
-        // Schedule the stats retrieval every second
         scheduler.scheduleAtFixedRate(() -> {
-            shardStateActor.tell(new GetClusterShardingStats(
-                    ChatRoomActor.TYPE_KEY,
-                    Duration.ofSeconds(3),
-                    metricsActors.getRef()
-            ));
-
             shardStateActor.tell(new GetShardRegionState(
-                    ChatRoomActor.TYPE_KEY,
-                    metricsActorsV2.getRef()
+                    HelloActor.TYPE_KEY,
+                    metricsActors.getRef()
             ));
         }, 0, 1, TimeUnit.SECONDS);
     }
